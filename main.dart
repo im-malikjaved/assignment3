f68f4409535877deee21a672439b83465e18d5e3
im-malikjaved <muhammadjaved444@gmail.com>
2022-03-10 23:59:27 +0500

    First initialize assignment 3

---
+++ main.dart
@@ -0,0 +1,33 @@
+void main() {
+  print("Assignmnet 3");
+  print("");
+  print("8 Types of Operator in Dart");
+  print("");
+  print("1:Arthmetic Operators");
+  print(
+      "Description: this class of operators contain those operators which are used to perform arithmetic operation on the operands. They are binary operators i.e they act on two operands. They go like this: ");
+  print("Ex 1: Addition (+) 2+2 = ${2 + 2}");
+  print("Ex 2: Subtraction (-) 4-2= ${4 - 2}");
+  print("Ex 3: Multiply (*) 4*2= ${4 * 2}");
+  print("Ex 4: Division (/): ${8 / 2}");
+  print("Ex 5: Modulus (%): ${8 % 2}");
+  print("");
+
+  print("2:Relational Operators");
+  print(
+      "Description: This class of operators contain those operators which are used to perform relational operation on the operands.");
+  print("Ex 1: Greater Than (>) 2 > 3 = ${2 > 3}");
+  print("Ex 2: Less Than (<) 2 < 3 = ${2 < 3}");
+  print(
+      "Ex 3: Greater than or equal to (>=) 3 >= 3 = ${3 >= 3} or 4>=3 = ${4 >= 3} ");
+  print("Ex 4: Greater Than (>) 2 > 3 = ${2 > 3}");
+  print("Ex 5: Greater Than (>) 2 > 3 = ${2 > 3}");
+  print("Ex 6: Greater Than (>) 2 > 3 = ${2 > 3}");
+
+  print("3:Type Test Operators");
+  print("4:Bitwise Operators");
+  print("5:Assignment Operators");
+  print("6:Logical Operators");
+  print("7:Conditional Operator");
+  print("8:Cascade Notation Operator");
+}
